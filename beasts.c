4b1db19340e723daf36d1b93e5bd5ae1ff04892b
Mike Watts <malphas@subdimension.co.uk>
2019-03-05 20:20:50 +0000

    reset beast before re-generating

--- beasts.c
+++ beasts.c
@@ -3,33 +3,36 @@
 void battle_beasts(void)
 {
     uint32_t t = 0;
-	uint32_t gen_timer = 0;
-	
-	uint8_t player_beast[8] = {
-		0x00,
-		0x00,
-		0x00,
-		0x00,
-		0x00,
-		0x00,
-		0x00,
-		0x00,
-	};
-	
+    uint32_t gen_timer = 0;
+    
+    uint8_t player_beast[8] = {
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+    };
+    
     for(ever)
     {
         t = millis();
-		
-		if (gen_timer <= t)
-		{
-			gen_timer = t+2000;
-			
-			generate_beast(player_beast);
-		}
+        
+        if (gen_timer <= t)
+        {
+            gen_timer = t+2000;
+            
+            for(uint8_t i=0 ; i<8 ; i++)
+                player_beast[i] = 0x00;
+            
+            generate_beast(player_beast);
+        }
         
         clear_buffer();
-		
-		draw_beast(player_beast);
+        
+        draw_beast(player_beast);
             
         draw();
     }
@@ -37,21 +40,21 @@ void battle_beasts(void)
 
 void generate_beast(uint8_t* beast)
 {
-	for(uint8_t y=0 ; y<7 ; y++)
-	{
-		for (uint8_t x=0 ; x<7 ; x++)
-		{
-			if (x < 4)
-				beast[x] |= (rng() & 1) << y;
-			else
-				if (beast[x] & (1 << (6-y)))
-					beast[x] |= 1 << y;
-		}
-	}
+    for(uint8_t y=0 ; y<7 ; y++)
+    {
+        for (uint8_t x=0 ; x<7 ; x++)
+        {
+            if (x < 4)
+                beast[x] |= (rng() & 1) << y;
+            else
+                if (beast[x] & (1 << (6-y)))
+                    beast[x] |= 1 << y;
+        }
+    }
 }
 
 void draw_beast(uint8_t* beast)
 {
-	for(uint8_t i=0 ; i<8 ; i++)
-		buffer[ (6*SCREEN_WIDTH+(2*8)) + i ] = beast[i];
+    for(uint8_t i=0 ; i<8 ; i++)
+        buffer[ (6*SCREEN_WIDTH+(2*8)) + i ] = beast[i];
 }
