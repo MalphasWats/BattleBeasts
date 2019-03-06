f5685f96fdf2f3aedd322034cb5c4706d8235e67
Mike <malphas@subdimension.co.uk>
2019-03-06 10:35:57 +0000

    initial attempt to display beast larger

--- beasts.c
+++ beasts.c
@@ -47,14 +47,36 @@ void generate_beast(uint8_t* beast)
             if (x < 4)
                 beast[x] |= (rng() & 1) << y;
             else
-                if (beast[6-x] & (1 << y))
-                    beast[x] |= 1 << y;
+                beast[x] |= (beast[6-x] & (1 << y)) << y;
         }
     }
 }
 
 void draw_beast(uint8_t* beast)
 {
-    for(uint8_t i=0 ; i<8 ; i++)
-        buffer[ (6*SCREEN_WIDTH+(2*8)) + i ] = beast[i];
+	// Resize beast (x2, 16x16)
+	//TODO: this can only double 8x8
+	uint8_t resize_buff[32];
+	
+	uint8_t x2, y2;
+	uint16_t temp = 0;
+    for (uint8_t i=0 ; i<16 ; i++) 
+    {
+        for (uint8_t j=0 ; j<16 ; j++) 
+        {
+            x2 = (uint8_t)((j*32769)>>16) ;
+            y2 = (uint8_t)((i*32769)>>16) ;
+            
+			temp |= (beast[x2] & (1 << y2)) << j;
+        }
+		//re-assemble temp into resize_buff
+		resize_buff[i] = temp>>8;
+		resize_buff[i+16] = temp & 0x0f;
+    }
+	
+    for(uint8_t i=0 ; i<16 ; i++)
+	{
+        buffer[ (5*SCREEN_WIDTH+(2*8)) + i ] = resize_buff[i];
+		buffer[ (6*SCREEN_WIDTH+(2*8)) + i ] = resize_buff[i+16];
+	}
 }
